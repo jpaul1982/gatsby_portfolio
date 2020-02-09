cdba7ae575ae191419540a75b4f743bc5783dec3
Paul Molnar <saintpaul55012@yahoo.com>
2020-02-08 18:18:15 -0600

    initial commit

---
+++ src/pages/about.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class About extends Component {
+
+    render() {
+        return (
+            <div>
+            <h1>A bit about me:</h1>
+            <p>
+                loremIncididunt magna dolore commodo est consectetur qui proident veniam excepteur laboris. Non reprehenderit in ea minim amet. 
+                Enim cupidatat ex anim elit pariatur. Cupidatat dolor est est fugiat commodo. Minim pariatur Lorem laboris ut excepteur. 
+                Aliqua consequat anim consectetur nisi Lorem consequat voluptate mollit sit. 
+                Dolor ipsum mollit excepteur aliquip non commodo.
+            </p>
+            </div>
+        )
+    }
+}
+
+export default About;
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,13 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Welcome to my portfolio site!</h1>
+    <p>This is a site for sharing my devleopment projects.</p>
+    <p>Take a look, be impressed, and call me to set up an interview!</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/page-2">Go to page 2</Link>
   </Layout>
 )
 
