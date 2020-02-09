b4ad3e41730c30e367eb2ccb81e18fe394a1fc88
Paul Molnar <saintpaul55012@yahoo.com>
2020-02-08 20:33:27 -0600

    about

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,7 +5,7 @@ class About extends Component {
     render() {
         return (
             <div>
-            <h1>A bit about me:</h1>
+            <h1>A little bit about me:</h1>
             <p>
                 loremIncididunt magna dolore commodo est consectetur qui proident veniam excepteur laboris. Non reprehenderit in ea minim amet. 
                 Enim cupidatat ex anim elit pariatur. Cupidatat dolor est est fugiat commodo. Minim pariatur Lorem laboris ut excepteur. 
